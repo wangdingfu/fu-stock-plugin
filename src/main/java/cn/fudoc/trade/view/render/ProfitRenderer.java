9681e0d7c10effa312e2f3a23da6673782ed331b
王定福 <wangdingfu1024@163.com>
2025-12-15 00:13:52 +0800

    我的持仓新增今日收益

--- src/main/java/cn/fudoc/trade/view/render/ProfitRenderer.java
+++ src/main/java/cn/fudoc/trade/view/render/ProfitRenderer.java
@@ -4,7 +4,6 @@ package cn.fudoc.trade.view.render;
 import cn.hutool.core.util.NumberUtil;
 import com.intellij.ui.JBColor;
 import com.intellij.ui.components.JBLabel;
-import org.apache.commons.lang3.StringUtils;
 
 import javax.swing.*;
 import java.awt.*;
@@ -21,20 +20,20 @@ public class ProfitRenderer extends JPanel {
         JBLabel titleLabel = new JBLabel(title);
         titleLabel.setForeground(JBColor.GRAY);
         titleLabel.setFont(titleLabel.getFont().deriveFont(11.0f));
-        valueLabel.setFont(valueLabel.getFont().deriveFont(15.0f).deriveFont(Font.BOLD));
+        valueLabel.setFont(valueLabel.getFont().deriveFont(16.0f).deriveFont(Font.BOLD));
         add(titleLabel);
         add(valueLabel);
-        titleLabel.setBorder(BorderFactory.createEmptyBorder(3, 20, 0, 12));
+        titleLabel.setBorder(BorderFactory.createEmptyBorder(5, 20, 3, 12));
         valueLabel.setBorder(BorderFactory.createEmptyBorder(3, 20, 3, 12));
     }
 
 
-    public void setValue(String value,boolean isColor) {
-        if(isColor){
+    public void setValue(String value, boolean isColor) {
+        if (isColor) {
             double aDouble = NumberUtil.parseDouble(value, 0.0);
             valueLabel.setText(aDouble > 0 ? "+" + value : value);
             valueLabel.setForeground(getTextColor(aDouble));
-        }else {
+        } else {
             valueLabel.setText(value);
         }
     }
